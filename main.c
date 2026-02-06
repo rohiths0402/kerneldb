7794e347e2d5d50b8b05a0ae71908d43af69ce23
Rohith <rohiths0402@gmail.com>
2026-02-06 13:09:01 +0530

    Day 1: Initial repo setup with README and skeleton main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <iostream>
+
+int main(){
+
+    
+    return 0;
+}
