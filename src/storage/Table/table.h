3128ee07daab417acfb357516f1667bca1287d4a
Rohith <rohiths0402@gmail.com>
2026-03-22 22:35:56 +0530

    feat(table): add B+ Tree index support to table layer

--- src/storage/Table/table.h
+++ src/storage/Table/table.h
@@ -4,19 +4,22 @@
 #include <stdint.h>
 #include "../buffer/buffer.h"
 #include "../page/page.h"
+#include "../index/index.h"
 #include "../common/intent.h"
 
-#define MAX_TABLE_NAME 64
-#define MAX_TABLES 16
-#define TABLE_DIR "data"
+#define MAX_TABLE_NAME   64
+#define MAX_TABLES       16
+#define TABLE_DIR        "data"
 
 typedef struct {
-    char name[MAX_TABLE_NAME];
-    int fd;
-    BufferPool pool;
-    uint32_t page_count;
-    ColumnDef col_defs[MAX_COLUMNS];
-    int col_count;
+    char        name[MAX_TABLE_NAME];
+    int         fd;
+    BufferPool  pool;
+    INDEX      *index;
+    uint32_t    page_count;
+    ColumnDef   col_defs[MAX_COLUMNS];
+    int         col_count;
+    int         index_col;
 } Table;
 
 typedef enum {
@@ -27,20 +30,15 @@ typedef enum {
     TABLE_ERROR
 } TableResult;
 
-void table_subsystem_init(void);
+void        table_subsystem_init(void);
 TableResult table_create(const Intent *intent);
-Table *table_open(const char *name);
-void table_close(Table *table);
+Table      *table_open(const char *name);
+void        table_close(Table *table);
 TableResult table_drop(const char *name);
-
 TableResult table_insert(Table *table, const Intent *intent);
-
-void table_scan(Table *table, const Intent *intent);
-
+void        table_scan(Table *table, const Intent *intent);
 TableResult table_delete(Table *table, const Intent *intent);
-
 TableResult table_update(Table *table, const Intent *intent);
+int         table_exists(const char *name);
 
-int table_exists(const char *name);
-
-#endif
+#endif /* TABLE_H */
