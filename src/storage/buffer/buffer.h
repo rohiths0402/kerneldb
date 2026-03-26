63b2a22fa26fbe1ab5bceea93320afec63eae485
Rohith <rohiths0402@gmail.com>
2026-03-26 15:44:36 +0530

    feat(buffer): add RWLock to BufferFrame for thread-safe access

--- src/storage/buffer/buffer.h
+++ src/storage/buffer/buffer.h
@@ -2,20 +2,21 @@
 #define BUFFER_H
 #include <stdint.h>
 #include "../page/page.h"
+#include "../WAL/wal.h"
 #define BUFFER_POOL_SIZE  7
 #define INVALID_PAGE_ID   UINT32_MAX
 
 typedef struct {
-    Page    *page;
+    Page *page;
     uint32_t page_id;
-    int      dirty;
+    int dirty;
     uint64_t lru_rank;
 } BufferFrame;
 
 typedef struct {
     BufferFrame frames[BUFFER_POOL_SIZE];
-    uint64_t    clock;
-    int         table_fd;
+    uint64_t clock;
+    int table_fd;
 } BufferPool;
 
 typedef enum {
@@ -23,6 +24,14 @@ typedef enum {
     BUF_ERROR,
     BUF_NOT_FOUND
 } BufResult;
+
+typedef struct {
+    Page *page;
+    uint32_t page_id;
+    int dirty;
+    uint64_t lru_rank;
+    RWLock  lock; 
+} BufferFrame;
 
 void buf_init(BufferPool *pool, int table_fd);
 
